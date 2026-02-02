3797a09c9235675e8a888f299ef9413c4284a8eb
Wect <2602710174@qq.com>
2026-02-02 20:26:08 +0800

    Day 113

---
+++ 383. 赎金信.ts
@@ -0,0 +1,13 @@
+function canConstruct(ransomNote: string, magazine: string): boolean {
+  const map = new Map<string, number>();
+  for (const char of magazine) {
+    map.set(char, (map.get(char) || 0) + 1);
+  }
+  for (const char of ransomNote) {
+    if (!map.has(char) || map.get(char) === 0) {
+      return false;
+    }
+    map.set(char, map.get(char)! - 1);
+  }
+  return true;
+};
