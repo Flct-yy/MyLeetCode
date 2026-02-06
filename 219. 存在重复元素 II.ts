730c259e4b822f4869634f9f991599ffb6c71221
Wect <2602710174@qq.com>
2026-02-06 19:48:01 +0800

    Day 117

---
+++ 219. 存在重复元素 II.ts
@@ -0,0 +1,12 @@
+function containsNearbyDuplicate(nums: number[], k: number): boolean {
+  const map = new Map<number, number>();
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(nums[i])) {
+      if (i - map.get(nums[i])! <= k) {
+        return true;
+      }
+    }
+    map.set(nums[i], i);
+  }
+  return false;
+};
