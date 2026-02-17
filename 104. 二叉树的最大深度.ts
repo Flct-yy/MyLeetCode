ec8dbb6a6de644881fd977c55feff39315030a1d
Wect <2602710174@qq.com>
2026-02-17 18:34:31 +0800

    Day 128

---
+++ 104. 二叉树的最大深度.ts
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+class TreeNode {
+  val: number
+  left: TreeNode | null
+  right: TreeNode | null
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+  }
+}
+
+function maxDepth(root: TreeNode | null): number {
+  // 空树直接返回深度 0
+  if (root === null) {
+    return 0;
+  }
+
+  let depth = 0;
+  const queue: TreeNode[] = [root];
+
+  while (queue.length > 0) {
+    const levelSize = queue.length;
+    for (let i = 0; i < levelSize; i++) {
+      const node = queue.pop()!;
+      
+      if (node.right) {
+        queue.unshift(node.right);
+      }
+      if (node.left) {
+        queue.unshift(node.left);
+      }
+    }
+    depth++;
+  }
+
+  return depth;
+}
