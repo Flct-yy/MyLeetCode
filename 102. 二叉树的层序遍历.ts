410666fed9b2b585ad65fc16e6d5cee6d5fd859f
Wect <2602710174@qq.com>
2026-02-25 16:52:14 +0800

    Day 136

---
+++ 102. 二叉树的层序遍历.ts
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+class TreeNode {
+  val: number
+  left: TreeNode | null
+  right: TreeNode | null
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+  }
+}
+
+function levelOrder(root: TreeNode | null): number[][] {
+  if (!root) {
+    return []
+  }
+  const queue: TreeNode[] = [root]
+  const result: number[][] = []
+  while (queue.length) {
+    const levelSize = queue.length
+    const level: number[] = []
+    for (let i = 0; i < levelSize; i++) {
+      const node = queue.shift()
+      if (!node) continue;
+      level.push(node.val)
+      if (node.left) {
+        queue.push(node.left)
+      }
+      if (node.right) {
+        queue.push(node.right)
+      }
+    }
+    result.push(level)
+  }
+  return result;
+};
