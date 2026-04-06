fe1009e4ab12db918f986aa7791c028cadd9bcbf
Wect <2602710174@qq.com>
2026-04-06 17:05:47 +0800

    Day 176

---
+++ 201. 数字范围按位与.ts
@@ -0,0 +1,10 @@
+function rangeBitwiseAnd(left: number, right: number): number {
+  let shift = 0;
+  // 找到公共前缀
+  while (left < right) {
+    left >>= 1;
+    right >>= 1;
+    ++shift;
+  }
+  return left << shift;
+};
