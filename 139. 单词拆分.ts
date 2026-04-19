bffbd898e17e6580b2e52a03269801ee37be09a4
Wect <2602710174@qq.com>
2026-04-19 10:47:55 +0800

    Day 185

---
+++ 139. 单词拆分.ts
@@ -0,0 +1,15 @@
+function wordBreak(s: string, wordDict: string[]): boolean {
+  const n = s.length;
+  const wordSet = new Set(wordDict);
+  const dp = new Array(n + 1).fill(false);
+  dp[0] = true;
+  for (let i = 1; i <= n; i++) {
+    for (let j = 0; j < i; j++) {
+      if (dp[j] && wordSet.has(s.substring(j, i))) {
+        dp[i] = true;
+        break;
+      }
+    }
+  }
+  return dp[n];
+};
